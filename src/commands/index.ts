e3f7cecda57616925ae18f82091fc40a96926ca7
Ali Hammoud <letsmailali.h@gmail.com>
2025-11-04 13:36:06 +0200

    🌟 feat: add onboarding command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,12 +2,21 @@ import { docsCommands } from './docs/index.js';
 import { guidesCommand } from './guides/index.js';
 import cacheMessages from './moderation/cache-messages.js';
 import { repelCommand } from './moderation/repel.js';
+import { onboardingCommand } from './onboarding/index.js';
 import { pingCommand } from './ping.js';
 import { tipsCommands } from './tips/index.js';
 import type { Command } from './types.js';
 
 export const commands = new Map<string, Command>(
-  [pingCommand, guidesCommand, docsCommands, tipsCommands, repelCommand, cacheMessages]
+  [
+    pingCommand,
+    guidesCommand,
+    docsCommands,
+    tipsCommands,
+    repelCommand,
+    cacheMessages,
+    onboardingCommand,
+  ]
     .flat()
     .map((cmd) => [cmd.data.name, cmd])
 );
