3de135649cd3b489cb74755247609a991957427a
Ali Hammoud <letsmailali.h@gmail.com>
2025-11-05 20:13:09 +0200

    🔨 refactor: remove unecessary return by switching the condition 'hasRoleC` -> `!hasRoleC`

--- src/events/auto-roles.ts
+++ src/events/auto-roles.ts
@@ -1,23 +1,22 @@
 import { Events } from 'discord.js';
 import { config } from '../env.js';
 import { createEvent } from '../util/events.js';
-import { memberHasRoles } from '../util/member.js';
+import { hasRoles } from '../util/member.js';
 
 export const autoRoleEvent = createEvent(
   {
     name: Events.GuildMemberUpdate,
   },
   async (_, newMember) => {
-    const hasRoleC = memberHasRoles(newMember, config.roleC);
-    if (hasRoleC) {
-      return;
-    }
-    const hasRequiredRoles = memberHasRoles(newMember, config.roleA, config.roleB);
-    if (hasRequiredRoles) {
-      try {
-        await newMember.roles.add(config.roleC);
-      } catch (error) {
-        console.error(`Failed to add roleC to ${newMember.user.tag}:`, error);
+    const hasRoleC = hasRoles(newMember, config.roleC);
+    if (!hasRoleC) {
+      const hasRequiredRoles = hasRoles(newMember, config.roleA, config.roleB);
+      if (hasRequiredRoles) {
+        try {
+          await newMember.roles.add(config.roleC);
+        } catch (error) {
+          console.error(`Failed to add roleC to ${newMember.user.tag}:`, error);
+        }
       }
     }
   }
