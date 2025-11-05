45ae96c3c3055669d3d8c065d5348e9294162aa7
Wiktoria Van Harneveldt <w.dev+git@mailbox.org>
2025-11-05 18:23:59 +0100

    chore: should not be part of the curren active commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,21 +2,12 @@ import { docsCommands } from './docs/index.js';
 import { guidesCommand } from './guides/index.js';
 import cacheMessages from './moderation/cache-messages.js';
 import { repelCommand } from './moderation/repel.js';
-import { onboardingCommand } from './onboarding/index.js';
 import { pingCommand } from './ping.js';
 import { tipsCommands } from './tips/index.js';
 import type { Command } from './types.js';
 
 export const commands = new Map<string, Command>(
-  [
-    pingCommand,
-    guidesCommand,
-    docsCommands,
-    tipsCommands,
-    repelCommand,
-    cacheMessages,
-    onboardingCommand,
-  ]
+  [pingCommand, guidesCommand, docsCommands, tipsCommands, repelCommand, cacheMessages]
     .flat()
     .map((cmd) => [cmd.data.name, cmd])
 );
