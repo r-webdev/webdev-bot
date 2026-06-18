1ec94786f293542a2a6ba9839aa0683dedc11b2f
Ali Hammoud <letsmailali.h@gmail.com>
2026-06-18 11:38:34 +0300

    feat: add member utils

--- src/util/member.ts
+++ src/util/member.ts
@@ -1,5 +1,32 @@
-import type { GuildMember } from 'discord.js';
+import {
+  type APIInteractionGuildMember,
+  type BaseInteraction,
+  GuildMember,
+  PermissionFlagsBits,
+  type User,
+} from 'discord.js';
+import { config } from '@/env.js';
 
-export const hasRoles = (member: GuildMember, ...roles: string[]): boolean => {
+export const hasAllRoles = (member: GuildMember, ...roles: string[]): boolean => {
   return roles.every((roleId) => member.roles.cache.has(roleId));
+};
+
+export const hasAnyRole = (member: GuildMember, ...roles: string[]): boolean => {
+  return roles.some((roleId) => member.roles.cache.has(roleId));
+};
+
+export const isUserInServer = (
+  target: User | GuildMember | null | APIInteractionGuildMember
+): target is GuildMember => {
+  return target instanceof GuildMember;
+};
+
+export const isUserModerator = (member: GuildMember, interaction: BaseInteraction): boolean => {
+  return (
+    hasAnyRole(member, ...config.roleIds.moderators) ||
+    member.permissions.has(
+      PermissionFlagsBits.Administrator | PermissionFlagsBits.ModerateMembers
+    ) ||
+    interaction.guild?.ownerId === member.id
+  );
 };
