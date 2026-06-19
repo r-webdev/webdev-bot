cb35e4863a8cb44dc944bf6da2692c35de69bdf0
Ali Hammoud <letsmailali.h@gmail.com>
2026-06-19 23:45:39 +0300

    feat: add oxfmt

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "oxfmt";
+import { defineConfig } from 'oxfmt';
 
 export default defineConfig({
-	printWidth: 80,
-	useTabs: false,
-	tabWidth: 2,
-	semi: true,
-	singleQuote: true,
-	trailingComma: "es5",
-	bracketSpacing: true,
-	arrowParens: "always",
-	ignorePatterns: ["node_modules", "dist", "build", "coverage", ".git"],
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  ignorePatterns: ['node_modules', 'dist', 'build', 'coverage', '.git'],
 });
