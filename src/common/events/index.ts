0562ab95dcf55d1e2e068516ce131c545fa960f1
Cake <letsmailali.h@gmail.com>
2026-07-04 18:48:06 +0300

    feat: remove just-ask auto-responder (#72)

--- src/common/events/index.ts
+++ src/common/events/index.ts
@@ -1,14 +1,12 @@
 import { guildCreateEvent } from '@/features/guild-create/index.js';
 import { hasVarEvent } from '@/features/has-var/index.js';
 import { interactionCreateEvent } from '@/features/interaction-create/index.js';
-import { justAskEvent } from '@/features/just-ask/just-ask.js';
 import { readyEvent } from '@/features/ready/index.js';
 import type { DiscordEvent } from './types.js';
 
 export const events: DiscordEvent[] = [
   readyEvent,
   guildCreateEvent,
-  justAskEvent,
   hasVarEvent,
   interactionCreateEvent,
 ].flat();
