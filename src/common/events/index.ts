cc77fca70441750ecb9a601a52f893fcb2d1b4b4
michal-skraburski <michal@skraburski.com>
2026-08-16 21:55:30 +0100

    fix: enable spam-detection & reportMessage

--- src/common/events/index.ts
+++ src/common/events/index.ts
@@ -3,10 +3,12 @@ import { hasVarEvent } from '@/features/has-var/index.js';
 import { interactionCreateEvent } from '@/features/interaction-create/index.js';
 import { readyEvent } from '@/features/ready/index.js';
 import type { DiscordEvent } from './types.js';
+import { spamDetection } from '@/features/spam-detection/index.js';
 
 export const events: DiscordEvent[] = [
   readyEvent,
   guildCreateEvent,
   hasVarEvent,
   interactionCreateEvent,
+  spamDetection,
 ].flat();
