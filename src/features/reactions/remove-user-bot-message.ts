2d71067b813805dcef5cc138d29062b530401e8e
Ali Hammoud <letsmailali.h@gmail.com>
2026-08-24 04:57:20 +0300

    feat: handle user reactions to remove bot message

---
+++ src/features/reactions/remove-user-bot-message.ts
@@ -0,0 +1,40 @@
+import type { ClientEvents, Events, GuildMember } from 'discord.js';
+import { UserBotMessagesService } from '@/services/user-bot-messages/user-bot-messages-service.js';
+
+const DELETE_EMOJIS = ['🗑️', '❌'];
+
+export const removeUserBotMessage: (
+  ...args: ClientEvents[Events.MessageReactionAdd]
+) => Promise<void> | void = async (reaction, user) => {
+  if (
+    reaction.emoji.name === null ||
+    !DELETE_EMOJIS.includes(reaction.emoji.name)
+  ) {
+    return;
+  }
+  if (reaction.message.author?.id !== user.client.user.id) {
+    return;
+  }
+  const guild = reaction.message.guild;
+  if (guild === null) {
+    return;
+  }
+  let member: GuildMember;
+  try {
+    member =
+      guild.members.cache.get(user.id) ?? (await guild.members.fetch(user.id));
+  } catch {
+    return;
+  }
+  try {
+    const deleted = await UserBotMessagesService.deleteUserBotMessage({
+      messageId: reaction.message.id,
+      user: member,
+    });
+    if (deleted) {
+      await reaction.message.delete();
+    }
+  } catch {
+    return;
+  }
+};
